464b590d65cf7c575ab5dc0c5bde2bea552a0a47
xonix97 <redredfingrr@gmail.com>
2026-06-14 14:25:23 +0530

    fix: per-monitor DPI awareness (crisp rendering on hi-dpi)

--- src/main.cpp
+++ src/main.cpp
@@ -180,6 +180,7 @@ static void initWebView() {
 }
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int nCmd) {
+  SetProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2);
   CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   WNDCLASSA wc{};
